13eeab6857546a35a8bb6cad571cfc417e534ae8
wakeupmh <wakeupmh@gmail.com>
2020-06-09 02:27:58 -0300

    fix(project): adjust cors headers

--- api/send.js
+++ api/send.js
@@ -4,6 +4,13 @@ import { Logger } from '../infrastructure'
 import { validateSchema, handleSuccessResponse, handleErrorResponse } from '../common'
 
 export default (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
   return Bluebird.resolve(req.body)
     .tap(validateSchema(mailSchema))
     .then(schema => sendMail(schema))
