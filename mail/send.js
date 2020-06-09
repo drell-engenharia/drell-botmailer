68ceea6c4de30dc3eaf3ea8d99e175cfd6b6ac50
wakeupmh <wakeupmh@gmail.com>
2020-06-08 22:40:19 -0300

    refactor(project): adjust credentials

--- mail/send.js
+++ mail/send.js
@@ -3,9 +3,9 @@ import { credentials } from '../config'
 
 const transporter = nodemailer.createTransport(
   {
-    host: "smtp.office365.com", // hostname
-    secure: false, // TLS requires secureConnection to be false
-    port: 587, // port for secure SMTP
+    host: credentials.host,
+    secure: credentials.secure,
+    port: credentials.port,
     tls: {
       ciphers:'SSLv3'
     },
@@ -14,13 +14,6 @@ const transporter = nodemailer.createTransport(
       pass: credentials.pass,
     }
   }
-  // {
-  //   service:'hotmail',
-  //   auth: {
-  //     user: credentials.user,
-  //     pass: credentials.pass,
-  //   }
-  // }
 )
 
 export const sendMail = async ({ text, subject, name, to}) => 
