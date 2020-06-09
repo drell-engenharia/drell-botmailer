397f4d24d193b5d1fda43c4c2ca6eec94f3a7604
wakeupmh <wakeupmh@gmail.com>
2020-06-09 02:08:37 -0300

    feat(api/send): add micro-cors

--- api/send.js
+++ api/send.js
@@ -2,11 +2,16 @@ import Bluebird from 'bluebird'
 import { sendMail, mailSchema } from '../mail'
 import { Logger } from '../infrastructure'
 import { validateSchema, handleSuccessResponse, handleErrorResponse } from '../common'
+import microCors from 'micro-cors'
 
-export default (req, res) => {
+const cors = microCors()
+
+const handler = (req, res) => {
   return Bluebird.resolve(req.body)
     .tap(validateSchema(mailSchema))
     .then(schema => sendMail(schema))
     .then(handleSuccessResponse(res))
     .catch(handleErrorResponse(res, Logger))
 }
+
+export default cors(handler)
